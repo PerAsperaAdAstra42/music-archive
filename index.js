f9cfde15d42d17a9c4c8b1f9961ad8b5f97b0654
PerAsperaAdAstra42 <simon.xprofile@gmail.com>
2023-09-28 23:25:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const ul = document.getElementById('ul')
+const findInput = document.getElementById('findInput')
+const testBtn = document.getElementById('testBtn')
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('data.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Ошибка при загрузке файла:', error);
+  }
+}
+
+const createLiElement = (liElement) => {
+  const li = document.createElement('li')
+  const serial = document.createElement('span')
+  const path = document.createElement('span')
+  const file = document.createElement('div')
+  const link = document.createElement('a')
+  serial.innerHTML = liElement.id
+  serial.classList.add("serial")
+  path.innerHTML = liElement.path
+  path.classList.add("path")
+  file.innerHTML = liElement.filename
+  file.classList.add("file")
+  link.innerHTML = 'Ссылка YouTube'
+  link.classList.add('link')
+  link.href = `https://www.youtube.com/results?search_query=${liElement.filename}`
+  li.append(serial, path, file, link)
+  li.classList.add("li")
+  return li
+}
+
+findInput.addEventListener('input', (e) => { render() })
+findInput.addEventListener('paste', (e) => { render() })
+
+testBtn.addEventListener('click', () => {
+  findInput.value = ''
+  render()
+})
+
+function render() {
+  fetchData()
+    .then(() => {
+      const removePromises = Array.from(ul.childNodes).map(element => {
+        return new Promise((resolve) => {
+          element.remove();
+          resolve();
+        });
+      });
+      return Promise.all(removePromises);
+    })
+    .then(() => {
+      return fetchData();
+    })
+    .then(data => {
+      data.forEach((liData) => {
+        const filename = liData.filename.toLowerCase()
+        const inputName = findInput.value.toLowerCase()
+        if (filename.includes(inputName)) {
+          ul.append(createLiElement(liData))
+        }
+      });
+    });
+}
+
+render()
